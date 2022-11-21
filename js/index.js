c28fcebfa68eac8afa29a27ec71c6ac254da6ba9
Nacho <dyrien7@gmail.com>
2022-11-21 18:38:29 -0300

    correciones generales

--- js/index.js
+++ js/index.js
@@ -9,26 +9,31 @@ const usuarios =[{
 
 }];
 const productos =[{
+    id: 1,
     nombre: "Antiparras",
     precio: 3000,
     img: "https://http2.mlstatic.com/D_NQ_NP_602923-MLA51992135455_102022-O.webp"
 },
 {
+    id: 2,
     nombre: "Aniparras de Aguas Abiertas",
     precio: 5000,
     img: "https://http2.mlstatic.com/D_NQ_NP_857843-MLA31042138634_062019-O.webp"
 },
 {
+    id: 3,
     nombre: "Clip de nariz",
     precio: 1500,
     img: "https://http2.mlstatic.com/D_NQ_NP_939273-MLA50455736186_062022-O.webp"
 },
 { 
+    id:4,
     nombre: "Tapones de oido",
     precio: 1500,
     img: "https://http2.mlstatic.com/D_NQ_NP_869335-MLA48456336271_122021-O.webp"
 },
 {
+    id:5,
     nombre: "Anti-Fog",
     precio: 1500,
     img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/069/256/products/liquido-antifog-dak-21-6ef1051ac4b77a10c616616308205500-1024-1024.png"
@@ -40,6 +45,7 @@ const recordarme = document.getElementById("recordarme");
 const btnlogin = document.getElementById("btnlogin");
 const cards = document.getElementById("cards");
 const elementosToggeables = document.querySelectorAll(".togeable");
+const compras = document.getElementById("carrito");
 
 function validarUsuario(user, pwd, datos){
     let encontrado = datos.find(datos=>datos.usuario === user);
@@ -79,10 +85,10 @@ function saludar(usuario){
     Toastify({
         text:   "Bienvenido " + usuario.name,
         position: "center",
-        gravity: "top",
+        gravity: "bottom",
         duration: 5000,
         }).showToast();
-        console.log(usuario.nombre);
+
 }
 function guardarDatos(datos, storage){
     const usuario ={
@@ -109,10 +115,25 @@ Productos.forEach(element => {
     <div class="card-body">
       <h5 class="card-title">${element.nombre}</h5>
       <p class="card-text">$${element.precio}</p>
-      <a href="#" class="btn btn-primary">Agregar al Carrito</a>
+      <button class="btn btn-primary" id="btn${element.id}">Agregar al Carrito</button> 
     </div>
   </div>`
   cards.innerHTML += html;
+  
+const btnAntiparras = document.getElementById("btn1");
+const btnAntiparras2 = document.getElementById("btn2");
+const btnClip = document.getElementById("btn3");
+const btnTapones = document.getElementById("btn4");
+const btnFog = document.getElementById("btn5");
+const carrito = [];
+btnAntiparras.addEventListener("click",(event)=>{
+    event.preventDefault();
+    carrito.push(productos[0]);
+    console.log(carrito);
+      
+    
+})
+
 });
 
 }
@@ -157,6 +178,7 @@ btnlogin.addEventListener("click", (event)=>{
                 }
                 mostarCards(productos);
                 mostrarinfo(elementosToggeables, "d-none");
+                
             }
            
     }
@@ -165,4 +187,9 @@ btnlogin.addEventListener("click", (event)=>{
 
 window.onload=() =>{
     Logeado()
+   
+
 }
+
+
+
